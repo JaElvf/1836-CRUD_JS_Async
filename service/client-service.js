1b4c661c3f3125d36937eb17d7002810fc98eacd
Javier <ing.javier.vf@gmail.com>
2022-12-05 17:16:07 -0400

    Primera parte del curso

--- service/client-service.js
+++ service/client-service.js
@@ -30,6 +30,46 @@ const crearNuevaLinea = (nombre, email) => {
 
 const table = document.querySelector("[data-table]");
 
+//Abrir http (método, url)
+
+//CRUD - Métodos HTTP
+//Create - POST
+//Read - GET
+//Update - PUT/PATCH
+//Delete - DELETE
+
+const listaClientes = () => {
+  const promise = new Promise ((resolve,reject) => {
+    const http = new XMLHttpRequest();
+    http.open("GET", "http://localhost:3000/perfil");
+
+    http.send();
+
+    http.onload = () => {
+      const response = JSON.parse(http.response);
+      if(http.status >= 400){
+        reject(response);
+      }else{
+        resolve(response);
+      }
+    };
+  });
+
+  return promise;
+}
+
+listaClientes().then((data) => {
+  data.forEach((perfil) => {
+    const nuevaLinea = crearNuevaLinea(perfil.nombre,perfil.email);
+    table.appendChild(nuevaLinea);
+  });
+}).catch((error) => alert("Ocurrió un error"));
+
+
+/*
+
+
+
 //Abrir http (método,url)
 
 // CRUD   - Métodos HTTP
@@ -68,3 +108,4 @@ listaClientes()
 
 // console.log(data);
 //
+*/
